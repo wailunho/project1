ba9ee882f50d4d168ca23ae6ded613f75bba43a8
nabilnathani10 <nabilnathani@gmail.com>
2013-01-13 03:17:11 -0800

    break scipt stream based on '\n' and ';'

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -2,14 +2,17 @@
 
 #include "command.h"
 #include "command-internals.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 #include <error.h>
+#include <string.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -18,6 +21,30 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
+  //printf("%s", (char *)(get_next_byte_argument)); 
+  int ch = get_next_byte(get_next_byte_argument);
+  int stream_size=0; 
+  char *buff = malloc(sizeof(get_next_byte_argument)+1);
+  while(ch != EOF) {
+    buff[stream_size]=ch;
+    stream_size++;
+    ch = get_next_byte(get_next_byte_argument); 
+  }
+  //printf("Size of the bytestream is %d\n", sizeof(get_next_byte_argument)); 
+  //printf("the buffer contains:\n%s", buff);
+  
+  char delims[] = "\n;";
+  char *result = NULL;
+  //int inst_count = 0;
+  result = strtok(buff, delims);
+  while( result != NULL) {
+    printf("%s\n", result);
+    //inst_count++;
+    result = strtok(NULL, delims);
+  }
+  //doesnt work when i free memory
+  //free(buff);
+
   error (1, 0, "command reading not yet implemented");
   return 0;
 }
