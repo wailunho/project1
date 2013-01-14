b855363d0ed312b59ea31fb56a390c3a78ea1fe8
Wai Lun Ho <arlonghk@gmail.com>
2013-01-13 23:53:33 -0800

    updated make_command_stream

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -30,8 +30,10 @@ enum token_type
    complete the incomplete type declaration in command.h.  */
 struct command_stream
 {
-	int (*next_byte)(void *);
-	void * next_byte_argument;
+	int (*byte)(void *);
+	void * argu;
+	int commandnum;
+	char* token;
 	
 };
 
@@ -42,55 +44,17 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  //printf("%s", (char *)(get_next_byte_argument)); 
-  int ch = get_next_byte(get_next_byte_argument);
-  int stream_size=0; 
-  int breakcount=0;
-  char *buff = checked_malloc(sizeof(get_next_byte_argument)+1);
-  while(ch != EOF) {
-    if( ch =='\n' || ch == ';'){
-    breakcount++;
-    }
-    buff[stream_size]=ch;
-    stream_size++;
-    ch = get_next_byte(get_next_byte_argument); 
-  }
-  //printf("Size of the bytestream is %d\n", sizeof(get_next_byte_argument)); 
-  //printf("the buffer contains:\n%s", buff);
-  //printf("the number of line breaks are: %d\n", breakcount); 
+  command_stream_t buff = checked_malloc(sizeof(struct command_stream));
+  buff->byte = get_next_byte;
+  buff->argu = get_next_byte_argument;
   
-  
-  char delims[] = "\n;";
-  char *result = NULL;
-  size_t result_size = sizeof(result);
-  char *lines[breakcount+1];
-  int numlines=0;
-  result = strtok(buff, delims);
-  while( result != NULL) {
-    lines[numlines] = result;
-    numlines++;
-    result = strtok(NULL, delims);
-  }
-  
-  //int i=0;
-  //for( ; i<numlines; i++){
-  //printf("%s\n", lines[i]);
-  //}
-  
-  //lines is an array of Cpointers to strings 
-  
-
-  //doesnt work when i free memory
-  //free(buff);
-
-  //error (1, 0, "command reading not yet implemented");
-  return 0;
+  return buff;
 }
 
 command_t
 read_command_stream (command_stream_t s)
 {
   /* FIXME: Replace this with your implementation too.  */
-  error (1, 0, "command reading not yet implemented");
+  
   return 0;
 }
