7d4e60291271c6e1f06045ba05156986542941af
Wai Lun Ho <arlonghk@gmail.com>
2013-01-15 18:39:30 -0800

    still working

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -31,11 +31,86 @@ enum token_type
 struct command_stream
 {
 	char* stream;
-	char* token;
-	int size;
-	enum token_type last_token;
-	enum token_type current_token;
+  int stream_size;
+  char* token;
+  int token_size;
+  int linenum;
+  enum token_type last_token;
+  enum token_type current_token;
+  int (*get_next_byte) (void *);
+  void *get_next_byte_argument;
 };
+
+void increase_stream_size(command_stream_t s)
+{
+    s->stream_size += 40;
+    s->stream = checked_realloc(s->stream, sizeof(char*) * s->stream_size);
+}
+
+void increase_token_size(command_stream_t s)
+{
+    s->token_size += 20;
+    s->token = checked_realloc(s->token, sizeof(char*) * s->token_size);
+}
+
+//command_stream_t get_token()
+
+command_stream_t get_token(command_stream_t buff)
+{
+  int numofchar = 0;
+  int ch = buff->get_next_byte(buff->get_next_byte_argument);
+  while(ch != EOF)
+  {
+    //remove whitespaces
+    if(ch == ' ' || ch == '\t')
+      continue;
+
+    //read a word
+    else if(('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z') ||
+       ('0' <= ch && ch <= '9') || ch == '!' ||
+       ch == '%' || ch == '+' || ch == ',' || ch == '-' || 
+       ch == '.' || ch == '/' || ch == ':' || ch == '@' || ch == '^')
+    {
+      buff->token[numofchar++] = ch;
+      while(ch = buff->get_next_byte(buff->get_next_byte_argument))
+      {
+          if(('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z') ||
+            ('0' <= ch && ch <= '9') || ch == '!' ||
+             ch == '%' || ch == '+' || ch == ',' || ch == '-' || 
+             ch == '.' || ch == '/' || ch == ':' || ch == '@' || ch == '^')
+          {
+            buff->token[numofchar++] = ch;
+          }
+          else
+            break;
+          if(buff->token_size <= numofchar)
+          {
+            increase_token_size(buff);
+            buff->token[numofchar] = '\0';
+            buff->current_token = WORD_T;
+          }
+      }
+
+    }
+    //remove whitespaces from the beginning of a command
+    else if(ch == ' ' || ch == '\t')
+      continue;
+    //a command is made
+    else if (ch == '\n')
+    {
+      buff->linenum++;
+      while(ch = buff->get_next_byte(buff->get_next_byte_argument) == '\n')
+      {
+         buff->linenum++;
+      }
+      continue;
+    }
+    else
+      ch = buff->get_next_byte(buff->get_next_byte_argument);  
+
+  }
+  return buff;
+}
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -45,19 +120,16 @@ make_command_stream (int (*get_next_byte) (void *),
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
   command_stream_t buff = checked_malloc(sizeof(struct command_stream));
-  buff->stream = checked_malloc(sizeof (get_next_byte_argument) +1);
-  buff->token = checked_malloc(30 * sizeof (char));
-  int ch = get_next_byte(get_next_byte_argument);
-  int stream_size = 0;
-  while(ch != EOF) 
-  {
-        buff->stream[stream_size]=ch;
-        stream_size++;
-        ch = get_next_byte(get_next_byte_argument); 
-  }
-  buff->stream[stream_size]=ch;
-  stream_size++;
-
+  buff->stream_size = 40;
+  buff->stream = checked_malloc(sizeof (char*) * buff->stream_size);
+  buff->token_size = 20;
+  buff->token = checked_malloc(sizeof(char*) * buff->token_size);
+  buff->linenum = 0;
+  buff->last_token = WORD_T;
+  buff->get_next_byte = get_next_byte;
+  buff->get_next_byte_argument = get_next_byte_argument;
+  
+  buff = get_token(buff);
   return buff;
 }
 
@@ -68,16 +140,20 @@ command_t
 read_command_stream (command_stream_t s)
 {
   /* FIXME: Replace this with your implementation too.  */
-        char *result = &(s->stream[0]);
+      /*  char *result = &(s->stream[0]);
   	while (*(result) != EOF)
  	{
  		printf("%c", *(result));
   		result++;
         }
-	
-	return 0;
+*/
+  int i = 0;
+  for(i; s->token[i] != '\0'; i++)
+  {
+    printf("%c", s->token[i]);
+  }
+
+  printf("\n%d\n", s->linenum);
+  return 0;
 }
-
-
-	
 
