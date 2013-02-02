27371b6317c360cec116c3f9e68b38a635d46ac9
Wai Lun Ho <arlonghk@gmail.com>
2013-02-02 15:30:47 -0800

    updated test cases

--- lab1-Skeleton/main.c
+++ lab1-Skeleton/main.c
@@ -56,15 +56,15 @@ main (int argc, char **argv)
   while ((command = read_command_stream (command_stream)))
     {
       if (print_tree)
-	{
-	  printf ("# %d\n", command_number++);
-	  print_command (command);
-	}
+    	{
+    	  printf ("# %d\n", command_number++);
+    	  print_command (command);
+    	}
       else
-	{
-	  last_command = command;
-	  execute_command (command, time_travel);
-	}
+    	{
+    	  last_command = command;
+    	  execute_command (command, time_travel);
+  	  }
     }
 
   return print_tree || !last_command ? 0 : command_status (last_command);
