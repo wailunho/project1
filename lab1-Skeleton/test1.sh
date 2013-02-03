b01ecbc7ae15c4d21d525e41b85f3cdb537b3334
Wai Lun Ho <arlonghk@gmail.com>
2013-02-03 15:32:35 -0800

    version for submitting

--- lab1-Skeleton/test1.sh
+++ lab1-Skeleton/test1.sh
@@ -1,34 +1,38 @@
 #testing for ;
-#clear; ls; echo good			#passed
+clear; ls; echo good			#passed
 
 #testing < and >
-#cat < test3 > test2			#passed
+echo this is a test > test3
+cat < test3 > test2			#passed
+rm test3
+rm test2
 
 #testing |
-#ls 	| echo 111			#passed
-#echo 666 | cat > test2   # passed
-#ls | sort					#passed
+ls 	| echo 111			#passed
+echo 666 | cat > test2   # passed
+ls | sort					#passed
 
 #testing &&
-#true && false && ls 			#passed
+true && false && ls 			#passed
 
-#echo aaa && echo ccc && 123321 && echo bbb    #passed
-#echo hi
-#echo hello
-#aaa && echo ggg		
+echo aaa && echo ccc && 123321 && echo bbb    #passed
+echo hi
+echo hello
+aaa && echo ggg		
 
 #testing ||
-#-1 || -1 || -1 || ls		    #passed
-#ccc || echo aaa || dd || echo bbb    #passed
-#echo hi
-#echo hello
+-1 || -1 || -1 || ls		    #passed
+ccc || echo aaa || dd || echo bbb    #passed
+echo hi
+echo hello
 
 #testing for multiple commands	#passed
-#echo hi
-#echo hello
+echo hi
+echo hello
 
-# : : 							#passed
+: : : 							#passed
 
-#testing for subshell command
-#(echo 11222 > test2	)		#passed
+testing for subshell command
+(echo 11222 > test2	)		#passed
+rm test2
 
